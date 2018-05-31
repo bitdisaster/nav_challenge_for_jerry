c460fa66c9f56fcdb064d395dea7ced064999ece
Jan Hannemann <jan.hannemann@outlook.com>
2018-05-31 12:37:07 -0700

    the challenge

---
+++ NavTest/A.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NavTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class A : Page
+    {
+        public A()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.Frame.Navigate(typeof(B));
+        }
+
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            if (App.Frame.CanGoBack)
+                App.Frame.GoBack();
+        }
+    }
+}
